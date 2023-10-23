497f871f02b11063de337f9886fd36895015cc58
Paras Dongre <126282296+THEPHD1331@users.noreply.github.com>
2023-10-23 19:08:38 +0530

    Delete StudentAPI-New directory

--- StudentAPI-New/src/main/java/com/SpringBootProject/dao/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SpringBootProject.dao;
-
-import com.SpringBootProject.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
